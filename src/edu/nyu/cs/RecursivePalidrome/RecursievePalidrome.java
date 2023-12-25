397952d6624dcafd613f4ac3176812d179f3e062
Yuhao Sheng <ys4689@nyu.edu>
2023-12-24 22:36:48 -0500

    commit

---
+++ src/edu/nyu/cs/RecursivePalidrome/RecursievePalidrome.java
@@ -0,0 +1,21 @@
+package edu.nyu.cs.RecursivePalidrome;
+
+// Example of recursive helper method
+public class RecursievePalidrome {
+
+    public static boolean isPalindrome(String s) {
+        return isPalindrome(s, 0, s.length() - 1);
+    }
+
+    public static boolean isPalindrome(String s, int low, int high) {
+        if (high <= low) return true; // Base case
+        else if (s.charAt(low) != s.charAt(high)) return false; // Base case
+        else return isPalindrome(s, low + 1, high - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is moon a palindrome? " + isPalindrome("moon"));
+        System.out.println("Is mom a palindrome? " + isPalindrome("mom"));
+        System.out.println("is aba a palindrome? " + isPalindrome("aba"));
+    }
+}
