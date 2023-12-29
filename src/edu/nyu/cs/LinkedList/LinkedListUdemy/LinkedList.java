9776db9cea9f59004633d63f87e94a4587c245d1
imyhalex <ys4689@nyu.edu>
2023-12-29 01:20:32 -0500

    commit

--- src/edu/nyu/cs/LinkedList/LinkedListUdemy/LinkedList.java
+++ src/edu/nyu/cs/LinkedList/LinkedListUdemy/LinkedList.java
@@ -301,20 +301,36 @@ public class LinkedList {
     }
 
     public void reverseBetween(int start, int end) {
+        // Check if the list is empty; if so, return immediately
         if (length == 0) return;
+    
+        // Create a dummy node and set it to point to the head of the list
         Node dummyNode = new Node(0);
         dummyNode.next = head;
+    
+        // Find the node just before the start of the section to be reversed
         Node previousNode = dummyNode;
-        for (int i = 0; i < start; i++) {
+        for (int i = 0; i < start - 1; i++) {
             previousNode = previousNode.next;
         }
+    
+        // 'currentNode' will initially point to the first node in the section to be reversed
         Node currentNode = previousNode.next;
-        for (int i = 0; i < end; i++) {
+    
+        // Reverse the nodes in the specified range
+        for (int i = 0; i < end - start; i++) {
+            // Identify the next node to be moved
             Node nodeToMove = currentNode.next;
+    
+            // Adjust 'currentNode.next' to skip over 'nodeToMove'
             currentNode.next = nodeToMove.next;
+    
+            // Place 'nodeToMove' right after 'previousNode'
             nodeToMove.next = previousNode.next;
             previousNode.next = nodeToMove;
         }
+    
+        // Update the head of the list in case it was affected by the reversal
         head = dummyNode.next;
     }
 }
