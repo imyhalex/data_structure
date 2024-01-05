30d275ae7d55e237a790ce10a7c90c980f5185d1
imyhalex <ys4689@nyu.edu>
2024-01-05 01:18:06 -0500

    commit

--- src/edu/nyu/cs/HashTable/Main.java
+++ src/edu/nyu/cs/HashTable/Main.java
@@ -6,6 +6,12 @@ public class Main {
         
         HashTable myHashTable = new HashTable();
 
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
         myHashTable.printTable();
     }
 }
